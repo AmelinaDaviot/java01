8d8e44b5e70b7405542a7add7582cef243e9d657
Amélina Daviot <amelina.daviot2021@campus-eni.fr>
2022-02-11 11:27:52 +0100

    Ajout méthode afficherTexte

--- TestGit/src/fr/eni/testgit/TestApp.java
+++ TestGit/src/fr/eni/testgit/TestApp.java
@@ -5,6 +5,15 @@ public class TestApp {
 	public static void main(String[] args) {
 		System.out.println("Coucou !!");
 		System.out.println("Re-Coucou !!");
+		afficherTexte();
+		
+		}
+	
+	public static void afficherTexte() {
+		System.out.println("Blabla !");
+		int a = 5, b = 3;
+		int resultat = a+b;
+		System.out.println("5 + 3 = " + resultat);
 
 	}
 
